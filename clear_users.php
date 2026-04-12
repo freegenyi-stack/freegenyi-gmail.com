e5cc97e7cb6067105705c08220d8eafaa270f1c4
freegenyi-stack <colloque.entrepreneuriat6@gmail.com>
2026-04-12 20:43:21 +0200

    Auth Fix: Import manquant db.php resolut dans clear_users

--- clear_users.php
+++ clear_users.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/config/app.php';
+require_once __DIR__ . '/config/db.php';
 
 try {
     DB::execute('SET FOREIGN_KEY_CHECKS = 0');
